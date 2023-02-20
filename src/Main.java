3a3ef8bae521e814417d5591c54760a8053f21c6
patrickoliveira <patrick17silva@hotmail.com>
2023-02-20 16:28:52 -0300

    add dominios

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso = new Curso();
+        curso.setTitulo("Java");
+        curso.setDescricao("Java 11 para iniciantes");
+        curso.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Desc Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(mentoria);
     }
 }
